481670e47b3c214061330c90647bd01ed086e17d
Eric munyi <ericmunyi361@gmail.com>
2025-11-28 00:37:45 +0300

    Fix bug: Added text to remove button and corrected logic

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 //Wait for HTML to fully load before running the setup
 document.addEventListener('DOMContentLoaded',  () => {
-    // console.log ('HTML is fully loaded and ready')
+    // 1.Select elements
+    
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
+    // 2. Define the function so it can use the variables above
+    function addTask () {
+        // Retrieve and trim the value
+        const taskText = taskInput.value.trim();
+        // Check if empty
+
+        if(taskText === "")  {
+        alert("Please enter a task");
+        return;
+        }
+        // create list item
+        const  listItem = document.createElement('li');
+        listItem.textContent = taskText;
+
+        // create Remove button
+        const removeButton = document.createElement('button');
+        removeButton.textContent = "Remove";
+        removeButton.className = "remove-btn";
+
+        // Assign onClick event to the remove button
+
+        removeButton.onclick = function() {
+            taskList.removeChild(listItem);
+        };
+
+        // Append everything
+        listItem.appendChild(removeButton);
+        taskList.appendChild(listItem);
+
+        // clear input
+        taskInput.value = "";
+
+     }
+    //  3.Attach Event Listeners
     addButton.addEventListener('click', addTask);
+
     taskInput.addEventListener('keypress', (event) => {
         if(event.key === 'Enter') {
             addTask();
         }
-    })
+    });
 });
 
-function addTask () {
-    const taskInput = document.getElementById('task-input');
-    const taskList = document.getElementById('task-list');
-    const taskText = taskInput.value.trim();
-
-    if(taskText === "")  {
-        alert("Please enter a task");
-        return;
-    }
-    const  listItem = document.createElement('li');
-    listItem.textContent = taskText;
-
-    const removeButton = document.createElement('button');
-    removeButton.className = "remove-btn";
-
-    removeButton.onclick = function() {
-        taskList.removeChild(listItem);
-    };
-
-    listItem.appendChild(removeButton);
-    taskList.appendChild(listItem);
-
-    taskInput.value = "";
-
-    }
- 
